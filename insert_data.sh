27cd5308904ee49bfd865e321c5c9026372e2202
Kanyisa Ntombini <71196826+Kanyisa-Ntombini@users.noreply.github.com>
2026-08-03 11:17:36 +0200

    update

--- insert_data.sh
+++ insert_data.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 echo hello world
+
+PSQL="psql -X --username=user --dbname=budget_app --no-align --tuples-only -c"
+cat money_spent.csv | while IFS="," read DATA AMOUNT NAME CATEGORY
+do
+echo $CATEGORY $AMOUNT
+# get category id
+CATEGORY_ID=$($PSQL "SELECT category_id FROM category_table WHERE category='$CATEGORY'")
+if [[ -z $CATEGORY_ID ]]
+then
+# insert major
+# get new major_id
+fi
+done
